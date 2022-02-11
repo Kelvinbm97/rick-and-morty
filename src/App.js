f39caf93d527152c764ea9a65b49ca5fa95aa33f
Jhornan <jhornankevinmallqui@gmail.com>
2022-02-11 14:39:00 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { Filtrar } from "./components/Filtrar";
+import { Card } from "./components/Card";
+import Navbar from './components/Navbar';
+import './App.scss';
 
 function App() {
+  const [personajes, setPersonajes] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [filter, setFilter] = useState("");
+
+  const url = "https://rickandmortyapi.com/api/character";
+
+  useEffect(() => {
+    const getPersonajes = async () => {
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        setPersonajes(data.results);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getPersonajes();
+  }, []);
+
+  const personjesFiltrados = personajes.filter((personaje) =>
+    personaje.name.toLocaleLowerCase().includes(filter.toLocaleLowerCase())
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Navbar></Navbar>
+
+      <Filtrar filter={filter} setFilter={setFilter} />
+
+
+
+      <section className="container_lista-personajes">
+        {loading ? (
+          <p>Cargando.....</p>
+        ) : personjesFiltrados.length > 0 ? (
+          personjesFiltrados.map((personaje) => (
+            <Card key={personaje.id} personaje={personaje} />
+          ))
+        ) : (
+          <p>
+            No se encontro el personaje con la busqueda{" "}
+            <strong>"{filter}"</strong>.
+          </p>
+        )}
+      </section>
+
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Navbar.scss";
+const Navbar = () => {
+  return (
+    <nav className="contenedor">
+      <div className="contenedor_titulo">
+        <a href="/">RICK Y MORTY</a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
